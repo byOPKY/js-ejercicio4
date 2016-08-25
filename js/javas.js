890aeabc85b1aab1155f1f2107a53ae372e1ab4a
byOPKY <orrillokarol@gmail.com>
2016-08-25 15:20:12 -0500

    subiendo ejercicios resueltos

---
+++ js/javas.js
@@ -0,0 +1,13 @@
+window.alert("Calcula el precio de tu llamada");
+var num1 =parseInt(prompt ("¿Cuantos minutos deseas hablar?"));
+var num2 =parseInt(prompt ("¿Cuanto te cuesta el minuto?"));
+var suma = num1 * num2;
+document.write("Tu llamada será a tan solo S/. " + suma);
+
+
+
+
+
+
+
+
